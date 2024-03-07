0e948efc095285241a5726109da3a582fd0c995a
王世民 <shiminw@outlook.com>
2024-03-08 01:32:43 +0800

    feat: init program (#1)
    * build: add Cytools.csproj
    
    * feat: init program

---
+++ src/Program.cs
@@ -0,0 +1,11 @@
+namespace Cytools;
+
+internal sealed class Program
+{
+    internal Program() { }
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
